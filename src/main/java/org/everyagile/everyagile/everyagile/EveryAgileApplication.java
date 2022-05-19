f9dcd44cb83fb5eeb55bb3e59a17fdcf12413cd6
yejipractice <twoye1193@ajou.ac.kr>
2022-05-19 14:05:33 +0900

    [feature] dev setting

---
+++ src/main/java/org/everyagile/everyagile/everyagile/EveryAgileApplication.java
@@ -0,0 +1,13 @@
+package org.everyagile.everyagile.everyagile;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EveryAgileApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EveryAgileApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/everyagile/everyagile/everyagile/EveryAgileApplicationTests.java
@@ -0,0 +1,13 @@
+package org.everyagile.everyagile.everyagile;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EveryAgileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
