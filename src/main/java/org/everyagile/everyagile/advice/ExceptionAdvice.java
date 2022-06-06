bd047d32e6da40fa2a14bb2e064b239ecedd60b9
yejipractice <twoye1193@ajou.ac.kr>
2022-06-06 16:24:57 +0900

    [feature] Exception Advice setting

---
+++ src/main/java/org/everyagile/everyagile/advice/ExceptionAdvice.java
@@ -0,0 +1,23 @@
+package org.everyagile.everyagile.advice;
+
+import lombok.RequiredArgsConstructor;
+import org.everyagile.everyagile.domain.response.CommonResult;
+import org.everyagile.everyagile.service.ResponseService;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+@RequiredArgsConstructor
+@RestController
+public class ExceptionAdvice {
+    private final ResponseService responseService;
+
+    @ExceptionHandler(Exception.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    protected CommonResult defaultException(HttpServletRequest request, Exception e) {
+        return responseService.getFailResult();
+    }
+}
